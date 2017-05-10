48c18c3dea53f2da0169a0b2149e288ec74119e6
Cathal Killeen <cathalkilleen@gmail.com>
2017-05-09 21:26:38 -0600

    added hash change on link click

--- js/agency.js
+++ js/agency.js
@@ -2,15 +2,58 @@
 
 (function($) {
     "use strict"; // Start of use strict
+    // variable for indicating autoscroll animation is currently taking place
+    var autoscroll = false;
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
+        event.preventDefault();
+        autoscroll = true;
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+        }, 1250, 'easeInOutExpo', function(){
+            window.location.hash = $anchor.attr('href');
+            autoscroll = false;
+        });
+    });
+
+    // prevent jump on hashchange event
+    $(window).bind('hashchange', function(event) {
         event.preventDefault();
     });
+    $(window).on('hashchange', function(event) {
+        event.preventDefault();
+    });
+
+    // detect direction user is scrolling -- WAAAAAY TOO EXPENSIVE
+    // $(window).bind('mousewheel', function(event) {
+    //     if (event.originalEvent.wheelDelta >= 0) {
+    //         console.log('Scroll up');
+    //         scroll = 'up';
+    //     }
+    //     else {
+    //         console.log('Scroll down');
+    //         scroll = 'down';
+    //     }
+    // });
+
+    // $(document).bind('scroll',function(e){
+    //     $('section').each(function(){
+    //         if(
+    //             //prevent this action while autoscroll animation is currently happening
+    //             !autoscroll
+    //             //begins before top
+    //             && $(this).offset().top < window.pageYOffset + 10
+    //             //but ends in visible area
+    //             //+ 10 allows you to change hash before it hits the top border
+    //             && $(this).offset().top + $(this).height() > window.pageYOffset + 10
+    //         ){
+    //             //console.log(window.location.hash);
+    //             window.location.hash = $(this).attr('id');
+    //         }
+    //     });
+    // });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
