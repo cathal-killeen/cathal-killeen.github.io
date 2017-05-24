9ae64244bbaa29a043c5d6e79fe1a3ec975588fd
Cathal Killeen <cathalkilleen@gmail.com>
2017-05-24 18:12:02 +0100

    added main-head component

---
+++ js/components/main.js
@@ -0,0 +1,11 @@
+// components from main page
+app.component('mainHead', {
+    templateUrl: '../templates/main/head.html',
+    controller: Controller
+});
+
+
+Controller.$inject = ['$scope'];
+function Controller($scope) {
+
+}
